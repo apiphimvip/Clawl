d8cd7c526a67f0cfd4172705d261dfc81ba8da42
apiphimvip <ophimmoi.com@gmail.com>
2024-05-17 15:10:42 +0700

    fsf

---
+++ src/Collector.php
@@ -0,0 +1,122 @@
+<?php
+
+namespace Ophim\Crawler\OphimCrawler;
+
+use Illuminate\Support\Facades\Log;
+use Intervention\Image\ImageManagerStatic as Image;
+use Illuminate\Support\Facades\Storage;
+
+class Collector
+{
+    protected $fields;
+    protected $payload;
+    protected $forceUpdate;
+
+    public function __construct(array $payload, array $fields, $forceUpdate)
+    {
+        $this->fields = $fields;
+        $this->payload = $payload;
+        $this->forceUpdate = $forceUpdate;
+    }
+
+    public function get(): array
+    {
+        $info = $this->payload['movie'] ?? [];
+        $episodes = $this->payload['episodes'] ?? [];
+
+        $data = [
+            'name' => $info['name'],
+            'origin_name' => $info['origin_name'],
+            'publish_year' => $info['year'],
+            'content' => $info['content'],
+            'type' =>  $this->getMovieType($info, $episodes),
+            'status' => $info['status'],
+            'thumb_url' => $this->getThumbImage($info['slug'], $info['thumb_url']),
+            'poster_url' => $this->getPosterImage($info['slug'], $info['poster_url']),
+            'is_copyright' => $info['is_copyright'],
+            'trailer_url' => $info['trailer_url'] ?? "",
+            'quality' => $info['quality'],
+            'language' => $info['lang'],
+            'episode_time' => $info['time'],
+            'episode_current' => $info['episode_current'],
+            'episode_total' => $info['episode_total'],
+            'notify' => $info['notify'],
+            'showtimes' => $info['showtimes'],
+            'is_shown_in_theater' => $info['chieurap'],
+        ];
+
+        return $data;
+    }
+
+    public function getThumbImage($slug, $url)
+    {
+        return $this->getImage(
+            $slug,
+            $url,
+            Option::get('should_resize_thumb', false),
+            Option::get('resize_thumb_width'),
+            Option::get('resize_thumb_height')
+        );
+    }
+
+    public function getPosterImage($slug, $url)
+    {
+        return $this->getImage(
+            $slug,
+            $url,
+            Option::get('should_resize_poster', false),
+            Option::get('resize_poster_width'),
+            Option::get('resize_poster_height')
+        );
+    }
+
+
+    protected function getMovieType($info, $episodes)
+    {
+        return $info['type'] == 'series' ? 'series'
+            : ($info['type'] == 'single' ? 'single'
+                : (count(reset($episodes)['server_data'] ?? []) > 1 ? 'series' : 'single'));
+    }
+
+    protected function getImage($slug, string $url, $shouldResize = false, $width = null, $height = null): string
+    {
+        if (!Option::get('download_image', false) || empty($url)) {
+            return $url;
+        }
+        try {
+            $url = strtok($url, '?');
+            $filename = substr($url, strrpos($url, '/') + 1);
+            $path = "images/{$slug}/{$filename}";
+
+            if (Storage::disk('public')->exists($path) && $this->forceUpdate == false) {
+                return Storage::url($path);
+            }
+
+            // Khởi tạo curl để tải về hình ảnh
+            $ch = curl_init($url);
+            curl_setopt($ch, CURLOPT_HEADER, false);
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+            curl_setopt($ch, CURLOPT_BINARYTRANSFER, 1);
+            curl_setopt($ch, CURLOPT_USERAGENT, "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36");
+            $image_data = curl_exec($ch);
+            curl_close($ch);
+
+            $img = Image::make($image_data);
+
+            if ($shouldResize) {
+                $img->resize($width, $height, function ($constraint) {
+                    $constraint->aspectRatio();
+                });
+            }
+
+            Storage::disk('public')->put($path, null);
+
+            $img->save(storage_path("app/public/" . $path));
+
+            return Storage::url($path);
+        } catch (\Exception $e) {
+            Log::error($e->getMessage());
+            return $url;
+        }
+    }
+}
---
+++ src/Controllers/CrawlController.php
@@ -0,0 +1,131 @@
+<?php
+
+namespace Ophim\Crawler\OphimCrawler\Controllers;
+
+
+use Backpack\CRUD\app\Http\Controllers\CrudController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Facades\Http;
+use Ophim\Crawler\OphimCrawler\Crawler;
+use Ophim\Core\Models\Movie;
+
+/**
+ * Class CrawlController
+ * @package Ophim\Crawler\OphimCrawler\Controllers
+ * @property-read \Backpack\CRUD\app\Library\CrudPanel\CrudPanel $crud
+ */
+class CrawlController extends CrudController
+{
+    public function fetch(Request $request)
+    {
+        try {
+            $data = collect();
+
+            $request['link'] = preg_split('/[\n\r]+/', $request['link']);
+
+            foreach ($request['link'] as $link) {
+                if (preg_match('/(.*?)(\/phim\/)(.*?)/', $link)) {
+                    $link = sprintf('%s/phim/%s', config('ophim_crawler.domain', 'https://apii.online/apii'), explode('phim/', $link)[1]);
+                    $response = json_decode(file_get_contents($link), true);
+                    $data->push(collect($response['movie'])->only('name', 'slug')->toArray());
+                } else {
+                    for ($i = $request['from']; $i <= $request['to']; $i++) {
+                        $response = json_decode(Http::timeout(30)->get($link, [
+                            'page' => $i
+                        ]), true);
+                        if ($response['status']) {
+                            $data->push(...$response['items']);
+                        }
+                    }
+                }
+            }
+
+            return $data->shuffle();
+        } catch (\Exception $e) {
+            return response()->json(['message' => $e->getMessage()], 500);
+        }
+    }
+
+    public function showCrawlPage(Request $request)
+    {
+        $categories = [];
+        $regions = [];
+        try {
+            $categories = Cache::remember('ophim_categories', 86400, function () {
+                $data = json_decode(file_get_contents(sprintf('%s/the-loai', config('ophim_crawler.domain', 'https://apii.online/apii'))), true) ?? [];
+                return collect($data)->pluck('name', 'name')->toArray();
+            });
+
+            $regions = Cache::remember('ophim_regions', 86400, function () {
+                $data = json_decode(file_get_contents(sprintf('%s/quoc-gia', config('ophim_crawler.domain', 'https://apii.online/apii'))), true) ?? [];
+                return collect($data)->pluck('name', 'name')->toArray();
+            });
+        } catch (\Throwable $th) {
+
+        }
+
+        $fields = $this->movieUpdateOptions();
+
+        return view('ophim-crawler::crawl', compact('fields', 'regions', 'categories'));
+    }
+
+    public function crawl(Request $request)
+    {
+        $pattern = sprintf('%s/phim/{slug}', config('ophim_crawler.domain', 'https://apii.online/apii'));
+        try {
+            $link = str_replace('{slug}', $request['slug'], $pattern);
+            $crawler = (new Crawler($link, request('fields', []), request('excludedCategories', []), request('excludedRegions', []), request('excludedType', []), request('forceUpdate', false)))->handle();
+        } catch (\Exception $e) {
+            return response()->json(['message' => $e->getMessage(), 'wait' => false], 500);
+        }
+        return response()->json(['message' => 'OK', 'wait' => $crawler ?? true]);
+    }
+
+    protected function movieUpdateOptions(): array
+    {
+        return [
+            'Tiến độ phim' => [
+                'episodes' => 'Tập mới',
+                'status' => 'Trạng thái phim',
+                'episode_time' => 'Thời lượng tập phim',
+                'episode_current' => 'Số tập phim hiện tại',
+                'episode_total' => 'Tổng số tập phim',
+            ],
+            'Thông tin phim' => [
+                'name' => 'Tên phim',
+                'origin_name' => 'Tên gốc phim',
+                'content' => 'Mô tả nội dung phim',
+                'thumb_url' => 'Ảnh Thumb',
+                'poster_url' => 'Ảnh Poster',
+                'trailer_url' => 'Trailer URL',
+                'quality' => 'Chất lượng phim',
+                'language' => 'Ngôn ngữ',
+                'notify' => 'Nội dung thông báo',
+                'showtimes' => 'Giờ chiếu phim',
+                'publish_year' => 'Năm xuất bản',
+                'is_copyright' => 'Đánh dấu có bản quyền',
+            ],
+            'Phân loại' => [
+                'type' => 'Định dạng phim',
+                'is_shown_in_theater' => 'Đánh dấu phim chiếu rạp',
+                'actors' => 'Diễn viên',
+                'directors' => 'Đạo diễn',
+                'categories' => 'Thể loại',
+                'regions' => 'Khu vực',
+                'tags' => 'Từ khóa',
+                'studios' => 'Studio',
+            ]
+        ];
+    }
+
+    public function getMoviesFromParams(Request $request) {
+        $field = explode('-', request('params'))[0];
+        $val = explode('-', request('params'))[1];
+        if (!$val) {
+            return Movie::where($field, $val)->orWhere($field, 'like', '%.com%')->orWhere($field, NULL)->get();
+        } else {
+            return Movie::where($field, $val)->get();
+        }
+    }
+}
